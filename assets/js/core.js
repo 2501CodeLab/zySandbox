c59a8bce183c677ef6cd6a2a55435f7126eab80a
Zyplos <officialzyplos@gmail.com>
2016-12-13 17:16:36 -0600

    Firebase Fix
    Some backend fixes.

--- assets/js/core.js
+++ assets/js/core.js
@@ -1,10 +1,9 @@
 var config = {
-      apiKey: "AIzaSyBm2JK3nv7T5tmKUeiEZXm32jJQmWd4Tuk",
-      authDomain: "zydisplay.firebaseapp.com",
-      databaseURL: "https://zydisplay.firebaseio.com",
-      storageBucket: "zydisplay.appspot.com",
-      messagingSenderId: "403802448125"
-  };
-  firebase.initializeApp(config);
-
-  console.log('%c>> CORE LOADED.', 'color: #ff3e3e;');
+    apiKey: "AIzaSyCXtXj2WUKlUqrSMbE37g1wYRF1jC6X-nI",
+    authDomain: "zysandbox.firebaseapp.com",
+    databaseURL: "https://zysandbox.firebaseio.com",
+    storageBucket: "zysandbox.appspot.com",
+    messagingSenderId: "100874609275"
+};
+firebase.initializeApp(config);
+console.log('%c>> CORE LOADED.', 'color: #ff3e3e;');
